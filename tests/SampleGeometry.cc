67c3756aa4c6f1ab39d5a1eb8c01a036d14a1d09
Daniel Winkelman <dwinkelman3@gmail.com>
2022-08-10 15:55:09 -0400

    [tests] Sample geometry of 2D planes

--- tests/SampleGeometry.cc
+++ tests/SampleGeometry.cc
@@ -46,5 +46,73 @@ std::vector<StlTriangle> geometry = {
     StlTriangle({v3, v6, v5}), StlTriangle({v5, v4, v3})};
 }  // namespace bowtie_
 std::vector<StlTriangle> bowtie(bowtie_::geometry);
+
+std::vector<std::vector<Vec2>> acuteTriangle = {{{0, 0}, {2, 0}, {1, 2}}};
+
+std::vector<std::vector<Vec2>> rightTriangle = {{{0, 0}, {2, 0}, {0, 2}}};
+
+std::vector<std::vector<Vec2>> obtuseTriangle = {{{0, 0}, {2, 1}, {-2, 1}}};
+
+std::vector<std::vector<Vec2>> convexPolygon = {
+    {{0, 0}, {2, 0}, {4, 1}, {6, 3}, {5, 5}, {2, 3}}};
+
+std::vector<std::vector<Vec2>> acuteConcavePolygon = {
+    {{0, 0}, {4, -1}, {2, 0}, {4, 1}}};
+
+std::vector<std::vector<Vec2>> rightConcavePolygon = {
+    {{0, 0}, {4, -1}, {3, 0}, {4, 1}}};
+
+std::vector<std::vector<Vec2>> obtuseConcavePolygon = {
+    {{0, 0}, {3, 0}, {1, 1}, {0, 3}}};
+
+std::vector<std::vector<Vec2>> straightAnglePolygon = {
+    {{0, 0}, {2, 0}, {4, 0}, {2, 3}}};
+
+std::vector<std::vector<Vec2>> disjointTriangles = {{{0, 0}, {2, 0}, {1, 2}},
+                                                    {{4, 4}, {2, 4}, {3, 2}}};
+
+std::vector<std::vector<Vec2>> twoTangentTriangles = {
+    {{0, 0}, {2, 0}, {1, 2}}, {{0, 0}, {-1, 2}, {-2, 0}}};
+
+std::vector<std::vector<Vec2>> threeTangentTriangles = {
+    {{0, 0}, {2, 0}, {1, 2}},
+    {{0, 0}, {-1, 2}, {-2, 0}},
+    {{0, 0}, {-1, -2}, {1, -2}}};
+
+std::vector<std::vector<Vec2>> doubleTangentPolygons = {
+    {{0, 0}, {2, 0}, {1, 1}, {2, 2}, {0, 2}},
+    {{2, 0}, {4, 0}, {4, 2}, {2, 2}, {3, 1}}};
+
+std::vector<std::vector<Vec2>> negativeTriangle = {{{0, 0}, {6, 0}, {3, 6}},
+                                                   {{2, 2}, {3, 4}, {4, 2}}};
+
+std::vector<std::vector<Vec2>> bubbleNegativeTriangle = {
+    {{0, 0}, {6, 0}, {3, 6}}, {{0, 0}, {3, 4}, {4, 1}}};
+
+std::vector<std::vector<Vec2>> twoTangentNegativeTriangles = {
+    {{0, 0}, {8, 0}, {4, 8}},
+    {{2, 1}, {3, 3}, {4, 1}},
+    {{4, 1}, {5, 3}, {6, 1}}};
+
+std::vector<std::vector<Vec2>> tangentPositiveAndNegativeTriangles = {
+    {{0, 0}, {6, 0}, {3, 6}},
+    {{0, 0}, {3, 4}, {4, 1}},
+    {{0, 0}, {-1, 2}, {-2, 0}}};
+
+std::vector<std::vector<Vec2>> sandwichedTriangles = {
+    {{0, 0}, {12, 0}, {6, 12}},
+    {{2, 1}, {6, 10}, {10, 1}},
+    {{4, 2}, {8, 2}, {6, 8}}};
+
+std::vector<std::vector<Vec2>> doubleSandwichedTriangles = {
+    {{0, 0}, {12, 0}, {6, 12}},
+    {{2, 1}, {6, 10}, {10, 1}},
+    {{4, 2}, {8, 2}, {6, 8}},
+    {{5, 3}, {6, 6}, {7, 3}}};
+
+std::vector<std::vector<Vec2>> bubblePositiveAndNegativeTriangles = {
+    {{0, 0}, {8, 0}, {4, 8}},
+    {{0, 0}, {4, 6}, {6, 2}},
+    {{0, 0}, {4, 4}, {4, 2}}};
 }  // namespace samples
 }  // namespace stl3lasercut
--- tests/SampleGeometry.h
+++ tests/SampleGeometry.h
@@ -8,8 +8,30 @@
 
 namespace stl3lasercut {
 namespace samples {
+// 3D geometry
 extern std::vector<StlTriangle> tetrahedron;
 extern std::vector<StlTriangle> octahedron;
 extern std::vector<StlTriangle> bowtie;
+
+// 2D non-overlapping geometry
+extern std::vector<std::vector<Vec2>> acuteTriangle;
+extern std::vector<std::vector<Vec2>> rightTriangle;
+extern std::vector<std::vector<Vec2>> obtuseTriangle;
+extern std::vector<std::vector<Vec2>> convexPolygon;
+extern std::vector<std::vector<Vec2>> acuteConcavePolygon;
+extern std::vector<std::vector<Vec2>> rightConcavePolygon;
+extern std::vector<std::vector<Vec2>> obtuseConcavePolygon;
+extern std::vector<std::vector<Vec2>> straightAnglePolygon;
+extern std::vector<std::vector<Vec2>> disjointTriangles;
+extern std::vector<std::vector<Vec2>> twoTangentTriangles;
+extern std::vector<std::vector<Vec2>> threeTangentTriangles;
+extern std::vector<std::vector<Vec2>> doubleTangentPolygons;
+extern std::vector<std::vector<Vec2>> negativeTriangle;
+extern std::vector<std::vector<Vec2>> bubbleNegativeTriangle;
+extern std::vector<std::vector<Vec2>> twoTangentNegativeTriangles;
+extern std::vector<std::vector<Vec2>> tangentPositiveAndNegativeTriangles;
+extern std::vector<std::vector<Vec2>> sandwichedTriangles;
+extern std::vector<std::vector<Vec2>> doubleSandwichedTriangles;
+extern std::vector<std::vector<Vec2>> bubblePositiveAndNegativeTriangles;
 }  // namespace samples
 }  // namespace stl3lasercut
